d969b34f6a3313d74c04b8835dc5ae985ea6edd9
Carla Pico <carlaspico@gmail.com>
2022-08-09 10:48:23 -0400

    getting beers from api

---
+++ src/components/beers/index.jsx
@@ -0,0 +1,32 @@
+import { toBeRequired } from '@testing-library/jest-dom/dist/matchers';
+import React, { useEffect, useState } from 'react';
+
+function Beers() {
+    const [beerList, setBeerList] = useState ();
+    useEffect(() => {
+        fetch(`https://api.sampleapis.com/beers/ale`) //ale, stouts, red-ale
+        .then(response => response.json()) 
+        .then(beers => setBeerList(beers))
+        .catch(alert)
+    },[])
+
+    if (!beerList) {
+        return (
+            <h2>Loading...</h2>
+        )
+    }
+
+    return (
+        <>
+            <ul>
+                {beerList.map (beer => {
+                    <li key= {beer.id}>
+                        {beer.name}
+                    </li>
+                })}
+            </ul>
+        </>
+    )
+}
+
+export default Beers;
